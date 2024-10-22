3d4ae8dcc979a706508a90c6fdde09ca85ffa327
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2024-10-22 14:15:58 +0900

    尋木祐翔

---
+++ admin/admintop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlayMate Admin</title>
+    <link rel="stylesheet" href="../css/admintop.css">
+</head>
+<body>
+    <h1>PlayMate Admin</h1>
+    <a href="login.html" class="logout">ログアウト</a>
+
+    <div>
+        <button class="menu-button" onclick="location.href='user-management.html'">ユーザー管理</button><br>
+        <button class="menu-button" onclick="location.href='game-manage.php'">ゲーム管理</button><br>
+        <button class="menu-button" onclick="location.href='tournament.php'">大会管理</button><br>
+        <button class="menu-button" onclick="location.href='contact.html'">お問い合わせ</button>
+    </div>
+</body>
+</html>
