c40f8e1d61e2654a14c2c3ec29a88dbbee5a5538
Shirai <2201222@s.asojuku.ac.jp>
2024-10-22 13:59:20 +0900

    pro

--- php/signup-output.php
+++ php/signup-output.php
@@ -4,21 +4,16 @@
       $pdo=new PDO($connect,USER,PASS);
 
       $lastId=$pdo->lastInsertId();
-      $sql=$pdo->prepare('insert into users(user_name, user_pass,'.
-                        'user_mail) values(?,?,?)');
-<<<<<<< HEAD
-      $sql->execute([$_POST['name'], $_POST['password'],$_POST['email']]);
+      // $sql=$pdo->prepare('insert into users(user_name, user_pass,'.
+      //                   'user_mail) values(?,?,?)');
+      // $sql->execute([$_POST['name'], password_hash($_POST['password'], PASSWORD_DEFAULT),$_POST['email']]);
       $user_name = htmlspecialchars($_POST['name']);
       $user_pass = htmlspecialchars($_POST['password']);
       $user_mail = htmlspecialchars($_POST['email']);
-=======
-      $sql->execute([$_POST['name'], password_hash($_POST['password'], PASSWORD_DEFAULT),$_POST['email']]);
->>>>>>> 25a70be08a777ba91aa49978686d77a6ba05b783
-
       header('Location:login-input.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,15 +22,18 @@
 <body>
 <div class="form-wrapper">
         <h1>Sign Up</h1>
-        <form action="signup-output.php" method="post">
+        <form action="signup-con.php" method="post">
             <div class="form-item">
-                <p>$user_name</p>
+                  <p>ユーザー名 <?= $user_name; ?></p>
             </div>
             <div class="form-item">
-                  <p>$user_pass</p>
+                  <p>パスワード <?= $user_pass; ?></p>
             </div>
             <div class="form-item">
-                  <p>$user_mail</p>
+                  <p>メールアドレス <?= $user_mail?></p>
+            </div>
+            <div>
+                <button type="submit" class="back" value="戻る" onclick=history.back()></button>
             </div>
             <div class="button-panel">
                 <input type="submit" class="button" title="Sign Up" value="Sign UP"></input>
