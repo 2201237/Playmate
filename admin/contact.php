d88d6cbe8fb469a72f04d0d13298e64b001e876e
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2024-10-23 10:37:35 +0900

    admin

--- admin/contact.php
+++ admin/contact.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ管理</title>
     <link rel="stylesheet" href="../user/css/game-manage.css">
-    <title>ゲーム管理</title>
 </head>
 <body>
     <a href="admintop.php" class="back">←戻る</a>
     <a href="login.php" class="logout">ログアウト</a>
 
-    <h2>ゲーム管理</h2>
+    <h2>お問い合わせ管理</h2>
 
     <div>
-        <button class="menu-button" onclick="location.href='game-title.html'">ゲームタイトル追加</button><br>
-        <button class="menu-button" onclick="location.href='game-title.html'">ゲームタイトル変更</button><br>
+        <button class="menu-button" onclick="location.href='inquiry-list.php'">お問い合わせ一覧</button><br>
+        <button class="menu-button" onclick="location.href='inquiry-solved.php'">お問い合わせ解決</button>
     </div>
 </body>
 </html>
