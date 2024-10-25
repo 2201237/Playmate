20976e26ca1a251734cd4c2e5e47060b525b3e17
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-10-25 10:26:48 +0900

    p

---
+++ admin/contacts.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../user/css/game-manage.css">
+    <title>大会管理</title>
+
+</head>
+<body>
+    <a href="admintop.php" class="back">←戻る</a>
+    <a href="login.php" class="logout">ログアウト</a>
+
+    <h2>お問い合わせ管理</h2>
+
+    <div>
+    <button class="menu-button" onclick="location.href='Contacts-List.html'">お問い合わせ一覧</button><br>
+        <button class="menu-button" onclick="location.href='Contacts-Resolution.html'">お問い合わせ解決</button><br>
+        <button class="menu-button" onclick="location.href='Genre-Management.html'">ジャンル管理</button>
+    </div>
+</body>
+</html>
