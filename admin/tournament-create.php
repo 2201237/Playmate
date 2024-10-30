117719bb281618a56ad1540b88e00768ce58c220
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-10-30 12:25:37 +0900

    o

--- admin/tournament-create.php
+++ admin/tournament-create.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/game-manage.css">
+    <link rel="stylesheet" href="css/tournament-create.css">
     <title>大会管理</title>
 
 </head>
