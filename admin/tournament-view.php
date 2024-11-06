a4cb454792e7efca36ac8a3f347dfb8573229a2a
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-11-06 10:56:36 +0900

    0

--- admin/tournament-view.php
+++ admin/tournament-view.php
@@ -5,35 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/tournament-view.css">
     <title>大会管理</title>
-
 </head>
 <body>
-    <a onclick="history.back()" class="back">←戻る</a>
+    <a href="tournament.php" class="back">←戻る</a>
     <a href="login.php" class="logout">ログアウト</a>
 
     <h2>大会一覧</h2>
     <?php
-require 'db-connect.php';
+    require 'db-connect.php';
 
-try {
-    $stmt = $pdo->query('SELECT tournament_name FROM tournament');
-    
-    // データがあるか確認
-    if ($stmt->rowCount() > 0) {
-        echo "<ul>";
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<li>" . htmlspecialchars($row['tournament_name']) . "</li>";
+    try {
+        // tournamentテーブルからtournament_nameを取得
+        $stmt = $pdo->query('SELECT tournament_name FROM tournament');
+
+        // データがあるか確認
+        if ($stmt->rowCount() > 0) {
+            echo "<ul>";
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo "<li>" . htmlspecialchars($row['tournament_name']) . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "データがありません。";
         }
-        echo "</ul>";
-    } else {
-        echo "データがありません。";
+
+    } catch (PDOException $e) {
+        echo 'データ取得エラー: ' . $e->getMessage();
     }
-
-} catch (PDOException $e) {
-    echo 'データ取得エラー: ' . $e->getMessage();
-}
-?>
-
-
+    ?>
 </body>
 </html>
