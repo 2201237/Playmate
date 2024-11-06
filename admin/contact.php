bd27363183dbf1142ade5d33653ec074843f924a
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2024-11-06 09:54:57 +0900

    admin

--- admin/contact.php
+++ admin/contact.php
@@ -15,7 +15,7 @@
     <div>
         <button class="menu-button" onclick="location.href='inquiry-list.php'">お問い合わせ一覧</button><br>
         <button class="menu-button" onclick="location.href='resolved-list.php'">お問い合わせ解決</button>
-        <button class="menu-button" onclick="location.href='inquiry-solved.php'">ジャンル管理</button>
+        <button class="menu-button" onclick="location.href='contacts_ge.php'">ジャンル管理</button>
     </div>
 </body>
 </html>
