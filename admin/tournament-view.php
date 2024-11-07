ec808a4d9398e4a3d654f086af3bb069f0a5074f
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-11-07 12:32:29 +0900

    9

--- admin/tournament-view.php
+++ admin/tournament-view.php
@@ -12,27 +12,11 @@
 
     <h2>大会一覧</h2>
     <div class="container">
-        <form action="your_action_page.php" method="post">
-            <input type="text" name="tournament-name" placeholder="大会名"><br>
-            <label for="game_title">ゲームタイトルを選択:</label>
-            <select name="game_title" id="game_title">
-                <?php
-                    // Database connection
-                    $pdo = new PDO($connect, USER, PASS);
-                    
-                    // Fetch game titles for dropdown
-                    $sql = $pdo->query('SELECT * FROM game');
-                    foreach ($sql as $row) {
-                        echo '<option value="', $row['game_id'], '">', $row['title'], '</option>';
-                    }
-                ?>
-            </select>
-        </form>
-
         <!-- Display tournament names -->
         <h3>登録済み大会名</h3>
         <ul>
             <?php
+                $pdo = new PDO($connect, USER, PASS);
                 // Fetch and display tournament names
                 $tournament_sql = $pdo->query('SELECT tournament_name FROM tournament');
                 foreach ($tournament_sql as $tournament) {
