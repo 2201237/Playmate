e907256cc6a1f0f1e038449497ed3961e291901d
ikenaga <133129091+t4gnu@users.noreply.github.com>
2024-11-08 11:07:37 +0900

    ログイン

--- user/php/login-output.php
+++ user/php/login-output.php
@@ -2,27 +2,31 @@
 <?php require 'db-connect.php'; ?>
 <?php
 
-  unset($_SESSION['User']);
+  //エラーメッセージをセッションに格納する処理
   $pdo=new PDO($connect, USER, PASS);
   $sql=$pdo->prepare('select * from users where user_mail=?');
   $sql->execute([$_POST['email']]);
-
-  if (empty($sql->fetchAll())) {
-    echo 'メールアドレスが間違っています';
+  if ( empty($sql->fetchAll()) ) {
+    $_SESSION['User']['message'] = 'このEメールアドレスを持つアカウントが見つかりません';
+    header('Location:login-input.php');
   }
 
   $sql=$pdo->prepare('select * from users where user_mail=?');
   $sql->execute([$_POST['email']]);
-
   foreach($sql as $row){
     if(password_verify($_POST['password'],$row['user_pass'])){
+      //パスワード一致
+      unset($_SESSION['User']);
       $_SESSION['User']=[
         'user_id'=>$row['user_id'],'user_name'=>$row['user_name'],
-        'user_mail'=>$row['user_mail'],'user_profile'=>$row['profile']
+        'user_mail'=>$row['user_mail'],'user_profile'=>$row['profile'],
+        'user_message'=>null
       ]; 
       header('Location:home.php');
     }else{
-      echo 'パスワードが違います';
+      //パスワード不一致
+      $_SESSION['User']['message'] = 'このEメールアドレスを持つアカウントが見つかりません';
+      header('Location:login-input.php');
     }
   }
 ?>
