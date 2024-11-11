5032ee4c54625d6c89f8764361d2f6ccf3dff26e
2201237 <2201237@s.asojuku.ac.jp>
2024-11-11 10:01:16 +0900

    Update tournament_chat.php

--- user/php/tournament_chat.php
+++ user/php/tournament_chat.php
@@ -13,18 +13,18 @@ try {
     }
     
     // フォームデータ取得
-    $user_id = $_SESSION['User']['user_id'];
-    $chat = $_POST['chat'];
+    // $user_id = $_SESSION['User']['user_id'];
+    // $chat = $_POST['chat'];
 
     // SQLクエリ作成
-    $sql = "INSERT INTO chat_tournament (user_id, tournament_id, round, chat) VALUES (:user_id, :tournament_id, :round, :chat)";
+    $sql = "INSERT INTO tournament_chat (user_id, tournament_id, round, chat) VALUES ('1', '1', '1', '')";
     $stmt = $pdo->prepare($sql);
 
     // パラメータバインド
-    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-    $stmt->bindParam(':tournament_id', $tournament_id, PDO::PARAM_INT);
-    $stmt->bindParam(':round', $round, PDO::PARAM_INT);
-    $stmt->bindParam(':chat', $chat, PDO::PARAM_STR);
+    // $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    // $stmt->bindParam(':tournament_id', $tournament_id, PDO::PARAM_INT);
+    // $stmt->bindParam(':round', $round, PDO::PARAM_INT);
+    // $stmt->bindParam(':chat', $chat, PDO::PARAM_STR);
 
     // クエリ実行
     $stmt->execute();
