acedf4effd43c34e25df62b5e3108edf95dfcb2d
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2024-11-12 11:00:37 +0900

    2

---
+++ user/php/ban.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/notlogin.css">
+    <title>BANされています</title>
+</head>
+<body>
+    <h1>あなたはBANされています</h1>
+    <p>3秒後にログインページにリダイレクトされます。</p>
+</body>
+<script>
+    // 3秒後にログインページにリダイレクト
+    setTimeout(function() {
+        window.location.href = 'login-input.php';
+    }, 3000);
+</script>
+</html>
