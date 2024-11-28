d24e0ae9f3fd69c71d7229713f759491a788c656
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2024-11-28 12:38:03 +0900

    1

---
+++ admin/game_change.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+// ログインチェック
+if (!isset($_SESSION['admins']['admin_id'])) {
+    header('Location: notlogin.php'); 
+    exit();
+}
+?>
+<?php require 'db-connect.php'?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/game_change.css">
+    <title>PlayMate Admin</title>                               
+</head>
+<body>
+    <div class="list_field">
+    <a href="#" onclick="history.back()" return false; class = "back1">◀戻る</a>
+    <h1>ゲームタイトル変更</h1>
+    <a href="logout.php" class="logout">ログアウト</a>
+    <form action="user_manage.php" method="post" id="submit_form">
+    <input type="text" name="keyword" placeholder="キーワードを検索" class = "searchBox">
+    <button type="submit" class="kennsaku">検索</button>
+    <div class="grouptable">
+    <table align="center" border="1">
+        <tr>
+            <th>ゲームタイトル</th>
+            <th></th>
+        </tr>
+<?php
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->query('SELECT * FROM game');
+foreach($sql as $row){
+
+    echo '<tr>';
+    echo '<td>', $row['title'], '</td>';
+    echo '</tr>';
+}
+
+?>
+</table>
+</div>
+</div>
+</body>
+</html>
