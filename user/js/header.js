cdb72008467a678323a2cc6d1ded0639b61eda61
2201237 <2201237@s.asojuku.ac.jp>
2024-12-03 13:15:48 +0900

    commit

--- user/js/header.js
+++ user/js/header.js
@@ -1,5 +1,9 @@
-$(function () {
-	$('.btn-gNav').on("click", function () {
-	  $('.gNav').toggleClass('open');  // メニューにopenクラスをつけ外しする
+// $(function () {
+// 	$('.btn-gNav').on("click", function () {
+// 	  $('.gNav').toggleClass('open');  // メニューにopenクラスをつけ外しする
+// 	});
+//   });
+	$('#hamburger').on('click', function(){
+	$('.icon').toggleClass('close');
+	$('.sm').slideToggle();
 	});
-  });
