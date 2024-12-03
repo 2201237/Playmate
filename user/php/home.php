35b16fdf3877d9911f0fa9527821c568edada3f7
ikenaga <133129091+t4gnu@users.noreply.github.com>
2024-12-03 10:59:34 +0900

    ホーム　ランキング表

--- user/php/home.php
+++ user/php/home.php
@@ -176,7 +176,7 @@
                                         '<img src="' .$win_lose['icon']. '">',
                                     '</div>
                                     <div class="icon">
-                                        <a href="#" class="iconBox"> <span class="material-symbols-outlined">
+                                        <a href="profile-partner.php?user_id='. $win_lose['user_id']. '"class="iconBox"> <span class="material-symbols-outlined">
                                             arrow_outward
                                         </span></a>
                                     </div>
@@ -185,7 +185,7 @@
                             <div class="content">
                                 <h3><img src="../img/rank_', $cnt ,'.png" class="f"><div class="name">', $win_lose['user_name'] ,'</div></h3>
                                 <p>',
-                                    '勝率：',round( $win_lose['wp'], 2 )*100,'%　',
+                                    '勝率：',round( $win_lose['wp'], 4 )*100,'%　',
                                     '試合数：'.$win_lose['user_games'].'　'.$win_lose['user_win'].'勝' .$lose_num. '敗<br>',
                                     $win_lose['profile'], '</p>
                                 <ul>',
@@ -201,16 +201,13 @@
     </section>
 
     <!-- ランキング表 -->
-    <div class="container">
+    <div class="container_not_3">
         <!-- Responsive Table Section -->
         <table class="responsive-table">
             <!-- Responsive Table Header Section -->
             <thead class="responsive-table__head">
             <tr class="responsive-table__row">
                 <th class="responsive-table__head__title responsive-table__head__title--name">Name
-                <svg version="1.1" class="up-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                    <path d="M374.176,110.386l-104-104.504c-0.006-0.006-0.013-0.011-0.019-0.018c-7.818-7.832-20.522-7.807-28.314,0.002c-0.006,0.006-0.013,0.011-0.019,0.018l-104,104.504c-7.791,7.829-7.762,20.493,0.068,28.285    c7.829,7.792,20.492,7.762,28.284-0.067L236,68.442V492c0,11.046,8.954,20,20,20c11.046,0,20-8.954,20-20V68.442l69.824,70.162c7.792,7.829,20.455,7.859,28.284,0.067C381.939,130.878,381.966,118.214,374.176,110.386z" />
-                </svg>
                 </th>
                 <th class="responsive-table__head__title responsive-table__head__title--status">Rank</th>
                 <th class="responsive-table__head__title responsive-table__head__title--types">Win Rate</th>
@@ -249,7 +246,7 @@
                                 $win_lose['user_name'],
                             '</td>
                             <td class="responsive-table__body__text responsive-table__body__text--status">',$cnt,'</td>
-                            <td class="responsive-table__body__text responsive-table__body__text--types">',round( $win_lose['wp'], 2 )*100,'%</td>
+                            <td class="responsive-table__body__text responsive-table__body__text--types">',round( $win_lose['wp'], 4 )*100,'%</td>
                             <td class="responsive-table__body__text responsive-table__body__text--update">試合数：'.$win_lose['user_games'].'　'.$win_lose['user_win'].'勝'.$lose_num.'敗</td>
                             <td class="responsive-table__body__text responsive-table__body__text--country">なし</td>
                         </tr>';
