edf250b9d58fbf55771d41c5285063bb2fb27c2c
ikenaga <133129091+t4gnu@users.noreply.github.com>
2024-12-04 10:17:10 +0900

    Update home.php

--- user/php/home.php
+++ user/php/home.php
@@ -8,6 +8,8 @@
 
     $rsql = $pdo->prepare('select * FROM ranking');
     $rsql->execute();
+
+    $iconPath = isset($_SESSION['User']['icon']) ? 'https://aso2201222.kill.jp/' . $_SESSION['User']['icon'] : '';
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +23,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/tournament-slide.css">
     <link rel="stylesheet" href="../css/board-slide.css">
-    <link rel="stylesheet" href="../css/ranking.css">
+    <link rel="stylesheet" href="../css/ranking-list.css">
     <link rel="stylesheet" href="../css/owl.carousel.min.css">
     <link rel="stylesheet" href="../css/owl.theme.default.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
@@ -33,8 +35,6 @@
 <body>
 
     <?php require 'header.php'; ?>
-
-
 
     <div class="headline">現在開催中の大会</div>
     <!-- 大会スライドショー -->
@@ -55,7 +55,7 @@
                                     <span>', $tournament['tournament_name'], '</span>
                                     <p>', $tournament['rule'], '</p>
                                 </div>
-                                <a href = "tournament-detail.php?tournament_id= '. $tournament['tournament_id'] . '">
+                                <a href = "tournament-join.php?tournament_id=" ', $tournament['tournament_id'], '">
                                     <div class="testimonial-name">
                                         <span class="name_text">詳細はこちら</span>
                                     </div>
@@ -72,7 +72,7 @@
 
 
 
-    <div class="headline">掲示板</div>
+    <div class='headline'>掲示板</div>
     <!-- 掲示板スライドショー -->
     <div class="wrap slide-paused">
         <ul class="slideshow">
@@ -128,85 +128,11 @@
 
 
 
-    <!--<p class="no_users">現在ユーザーが登録されていません</p>-->
-    <div class="headline">ランキング</div>
-    <!-- ランキング表 -->
-    <section>
+    <div class='headline'>ランキング</div>
+    
+    <!-- TOP3を表示する処理 -->
+    <section class="ranking_top3">
         <div class="container">
-<<<<<<< HEAD
-            <div class="card">
-            <div class="card-inner" style="--clr:#fafafa;">
-                <div class="box">
-                <div class="imgBox">
-                    <img src="../img/tnk.jpg" alt="Trust & Co.">
-                </div>
-                <div class="icon">
-                    <a href="#" class="iconBox"> <span class="material-symbols-outlined">
-                        arrow_outward
-                    </span></a>
-                </div>
-                </div>
-            </div>
-            <div class="content">
-                <h3><img src="../img/1st.png" class="f"><div class="name">バスケットマン3号！！！</div></h3>
-                <p>
-                    90勝5敗<br>
-                    俺以外、雑魚 　GG</p>
-                <ul>
-                <li style="--clr-tag:#d3b19a;" class="branding">にゃんこ大戦争（クソゲー）</li>
-                <li style="--clr-tag:#70b3b1;" class="packaging">マリオカート 8DX</li>
-                </ul>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-inner" style="--clr:#fafafa;">
-                <div class="box">
-                <div class="imgBox">
-                    <img src="../img/srt.png" alt="Tonic">
-                </div>
-                <div class="icon">
-                    <a href="#" class="iconBox"> <span class="material-symbols-outlined">
-                        arrow_outward
-                    </span></a>
-                </div>
-                </div>
-            </div>
-            <div class="content">
-                <h3><img src="../img/2nd.png" class="f"><div class="name">SRtama</div></h3>
-                <p>ほっともっと新宮店</p>
-                <ul>
-                <li style="--clr-tag:#d3b19a;" class="branding">にゃんこ大戦争（クソゲー）</li>
-                <li style="--clr-tag:#d05fa2;" class="marketing">League of Legends</li>
-                </ul>
-            </div>
-            </div>
-            <div class="card">
-            <div class="card-inner" style="--clr:#fafafa;">
-                <div class="box">
-                <div class="imgBox">
-                    <img src="../img/nhm.jfif" alt="Shower Gel">
-                </div>
-                <div class="icon">
-                    <a href="#" class="iconBox"> <span class="material-symbols-outlined">
-                        arrow_outward
-                    </span></a>
-                </div>
-                </div>
-            </div>
-            <div class="content">
-                <h3><img src="../img/3rd.png" class="f"><div class="name">なまはむ</div></h3>
-                <p>アンベッサアンチ　基本ロームしません</p>
-                <ul>
-                <li style="--clr-tag:#d3b19a;" class="branding">League of Legends</li>
-                <li style="--clr-tag:#70b3b1;" class="packaging">モンスターハンターライズ</li>
-                </ul>
-            </div>
-            </div>
-        </div>
-    </section>
-
-
-=======
             <?php
                 //試合数と勝利数、勝率を取得する処理
 
@@ -259,11 +185,12 @@
                             <div class="content">
                                 <h3><img src="../img/rank_', $cnt ,'.png" class="f"><div class="name">', $win_lose['user_name'] ,'</div></h3>
                                 <p>',
+                                    '<span class="top3_prf">',
+                                    $win_lose['profile'], '</span>',
                                     '勝率：',round( $win_lose['wp'], 4 )*100,'%　',
-                                    '試合数：'.$win_lose['user_games'].'　'.$win_lose['user_win'].'勝' .$lose_num. '敗<br>',
-                                    $win_lose['profile'], '</p>
+                                    '試合数：'.$win_lose['user_games'].'　'.$win_lose['user_win'].'勝' .$lose_num. '敗</p>
                                 <ul>',
-                                '<li style="--clr-tag:#d3b19a;" class="branding">大乱闘スマッシュブラザーズ</li>
+                                '<li style="--clr-tag:#d3b19a;" class="branding">League of Legends</li>
                                 <li style="--clr-tag:#70b3b1;" class="packaging">マリオカート 8DX</li>
                                 </ul>
                             </div>
@@ -331,9 +258,8 @@
         </table>
     </div>
     
->>>>>>> cd0bb936e7dcc76c789ffb2bce7adc380da5d1ff
     <script src="../js/header.js"></script>
     <script src="../js/home.js"></script>
-    <script src="../js/ranking.js"></script>
+    <script src="../js/ranking-list.js"></script>
 </body>
 </html>
